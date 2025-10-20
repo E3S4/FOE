b61bf97909d56c005ab95e25d64f5a6429858e48
E3S4 <eesak490@gmail.com>
2025-10-20 16:33:30 +0530

    changed dir related probs

--- ui/core/import.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "framebuffer.h"
-#include <QString>
-
-VideoClip importVideo(const QString &path);
-
--- ui/core/render.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "framebuffer.h"
-#include <string>
-
-void renderVideo(VideoClip clip, const std::string &outPath, bool applyFX = true);
-
